e48343dad133baaeb103b96924e704e61c5cc7f1
Duxlin <folorunshoabdulquodir1@gmail.com>
2024-09-04 23:42:15 +0100

    rxhl

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['2347082252014']
+global.botname = 'Dux Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Dux Official"
+global.sticker2 = "🌜"
